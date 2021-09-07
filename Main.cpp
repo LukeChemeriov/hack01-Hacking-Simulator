9dc15014e883e7a155a6a3304e70d530302f5daa
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-09-07 14:07:08 +0100

    Update Main.cpp
    Finished v1

--- Main.cpp
+++ Main.cpp
@@ -5,15 +5,16 @@
 
 #include <iostream> /* Don't be alarmed if I use printf, iostream include stdio */
 #include <windows.h>
+#include <conio.h>
 
 /* *gulps* Here we go... there's no turning back now..  */
 int main() {
-    int continueValue = MessageBox(NULL, L"This hacking tool has the power to cause severe damage. Creator MD DEV is not responsible for any damage caused using this tool. Continue?", L"WARNING - POWERFUL TOOL", MB_OKCANCEL | MB_ICONWARNING | MB_APPLMODAL); /* haven't tested this, don't know if this works */ 
-    if (continueValue == 2) {
-        return -1;
-    }
-    else {
-        
+    int continueValue = MessageBox(NULL, L"This hacking tool has the power to cause severe damage. Creator DDC DEV is not responsible for any damage caused using this tool. Continue?", L"WARNING - POWERFUL TOOL", MB_OKCANCEL | MB_ICONWARNING | MB_APPLMODAL); /* haven't tested this, don't know if this works */ 
+    switch(continueValue) {
+    case ID_OK:
+        printf("starting.........");
+    case ID_CANCEL:
+        return 0;
     }
     
 
@@ -23,8 +24,22 @@ printf ("≈≈≈≈≈≈≈≈\n");
 printf ("\n\nEnter target IP: >>>");
 std::string ipaddress;
 std::cin >> ipaddress;
-/* All I have time for, stay tuned! */
-
+printf("Hacking target IP...");
+Sleep(2000);
+printf("Forcing network disconnect...");
+Sleep(4000);
+printf("Connecting to " + ipadress + " for man-in-the-middle attack...");
+Sleep(1000);
+printf("Placing Keylogger...");
+Sleep(3000);
+printf("Extracting passwords...");
+Sleep(10000);
+printf("Device name: RBEKG-539639\nUsername: User\nPassword: gjGNGJYk58683");
+printf("======================================");
+printf("Press any key to terminate application");
+extern int _getchar();
+int uselessvaluethatwonteverbeused = _getchar()
+return 0;
 
 
 }
